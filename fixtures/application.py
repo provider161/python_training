8290e554db6880eb9f76baf284bcf9b4e7b59bf2
provider8 <provider8@yandex.ru>
2017-10-20 18:34:31 +0300

    добавлен класс для работы с группами

--- fixtures/application.py
+++ fixtures/application.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixtures.session import SessionHelper
+from fixtures.group import GroupHelper
 
 class Application:
 
@@ -7,31 +8,7 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def new_group_creation(self, Group):
-        wd = self.wd
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # filling group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(Group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(Group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(Group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
+        self.group = GroupHelper(self)
 
     def open_homepage(self):
         wd = self.wd
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -14,15 +14,15 @@ def app(request):
 def test_test_add_group(app):
     app.open_homepage()
     app.session.login(username = "admin", password = "secret")
-    app.open_groups_page()
-    app.new_group_creation(Group(name = "group1", header = "skip", footer = "skip"))
-    app.return_to_groups_page()
+    app.group.open_groups_page()
+    app.group.creation(Group(name ="group1", header ="skip", footer ="skip"))
+    app.group.return_to_groups_page()
     app.session.logout()
 
 def test_test_add_empty_group(app):
     app.open_homepage()
     app.session.login(username = "admin", password = "secret")
-    app.open_groups_page()
-    app.new_group_creation(Group(name = "", header = "", footer = ""))
-    app.return_to_groups_page()
+    app.group.open_groups_page()
+    app.group.creation(Group(name ="", header ="", footer =""))
+    app.group.return_to_groups_page()
     app.session.logout()
