92d340b7a124da3f268c041a8de1bd132fe33045
provider8 <provider8@yandex.ru>
2017-12-17 13:04:32 +0300

    Реализована генерация отчётов о выполнении тестов для адресной книги с веб-интерфейсом, используя генератор отчетов Allure.

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,15 +1,19 @@
 
 from models.Group import Group
+import pytest
 
 def test_add_group(app, json_group, db, check_ui):
     group = json_group
     app.group.open_groups_page()
-    old_groups = db.get_group_list()
-    app.group.creation(group)
-    app.group.return_to_groups_page()
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('When I add the group %s to the list' % group):
+        app.group.creation(group)
+        app.group.return_to_groups_page()
+    with pytest.allure.step('Then the new group list is equal to the old with added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
     app.open_homepage()
